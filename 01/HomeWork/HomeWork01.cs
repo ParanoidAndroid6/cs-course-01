83390d880f7b8d124415dd29d47c51ec46b69920
Yura <kachiko888100@gmail.com>
2019-04-18 14:06:09 +0300

    Home work of the 1st lesson added

---
+++ 01/HomeWork/HomeWork01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000); 
+            Console.WriteLine("Hello World!");
+            Thread.Sleep(5000);
+            Console.WriteLine("Bye!");
+
+
+            Console.ReadLine();
+      
+                }
+    }
+}
