f8dd45042059e9344c61843968195a841386beb0
Yura <kachiko888100@gmail.com>
2019-04-26 01:55:44 +0300

    HomeWork 3

---
+++ 03/HomeWork/ArrayHome1/ArrayHome/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace ArrayHome
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write names of three people:");
+       
+            string[] names = new string[3];
+            
+                for (int i = 0; i < names.Length; i++)
+            {
+               names[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Their age:");
+
+            int[] age = new int[3];
+            for (int i = 0; i < age.Length; i++)
+            {
+                age[i] = int.Parse(Console.ReadLine());   
+            }
+            Console.WriteLine("Name: " + names[0] + ", age in 4 years:" + (age[0] + 4));
+            Console.WriteLine("Name: " + names[1] + ", age in 4 years:" + (age[1] + 4));
+            Console.WriteLine("Name: " + names[2] + ", age in 4 years:" + (age[2] + 4));
+
+
+
+            Console.ReadKey();
+        }
+    }
+} 
