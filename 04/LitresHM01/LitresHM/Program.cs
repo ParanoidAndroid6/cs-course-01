6f55154fae1d44ab9e833ee98121cfce52d43cf6
Yura <kachiko888100@gmail.com>
2019-05-07 02:44:17 +0300

    HM05

---
+++ 04/LitresHM01/LitresHM/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LitresHM
+{
+    class Program
+    {
+        enum Litres: int 
+        {    
+            None = 0x0,
+            Litres1 = 0x1,
+            Litres5 = 0x1 << 1,
+            Litres20 = 0x1 << 2
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much litres of juice do u want?: ");
+            double input = Convert.ToDouble(Console.ReadLine());
+            var non = Convert.ToInt16(Litres.Litres5);
+            Litres lit = Litres.Litres1 | Litres.Litres5 | Litres.Litres20;
+            double num20 = Convert.ToDouble(Litres.Litres20);
+            double num05 = Convert.ToDouble(Litres.Litres5);
+            double divide20 = input / num20;
+            double divide20Smth = input % num20;
+            double divided = divide20Smth / non;
+            Litres divide5 = (Litres)input & Litres.Litres1;
+            
+            Console.WriteLine($"20 Litres: {Math.Floor(divide20)}");
+            Console.WriteLine($"20% Litres: {Math.Ceiling(divide20Smth)}");
+            Console.WriteLine($"20& Litres: {divide5}");
+            Console.ReadKey();
+        }
+    } 
+}
---
+++ 05/ColorsHM1/ColorsHM/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ColorsHM
+{
+    class Program
+    { enum Figures : short
+        { None = 0,
+            Circle,
+            Triangle,
+            Rectangle,
+
+        }
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Enter the type of figure: ");
+            var input = Convert.ToInt16(Console.ReadLine());
+
+            try
+            {
+
+
+                if (input == 1)
+                {
+                    Console.WriteLine("Please, set the diametre: ");
+                    var diam = Convert.ToInt16(Console.ReadLine());
+                    var circCal = Math.Sqrt(diam) * 3.1415;
+                    Console.WriteLine($"S: {circCal}");
+                    Console.WriteLine($"P: {diam * (2 * 3.1415)}");
+                }
+
+
+                if (input == 2)
+
+                {
+                    Console.WriteLine("Please set the side lenghth: ");
+                    var side = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Please set the height: ");
+                    var h = Convert.ToInt16(Console.ReadLine());
+                    var sideCal = (h * side) / 2;
+                    Console.WriteLine($"S: {sideCal}");
+                    Console.WriteLine($"P: {side * 3}");
+                }
+
+                if (input == 3)
+                {
+                    Console.WriteLine("Please set the side lenghth: ");
+                    var side = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Please set the width: ");
+                    var wide = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine($"S: {side * wide}");
+                    Console.WriteLine($"P: {(side + wide) * 2}");
+                }
+
+                
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Error. You need to set a number");
+            }
+            Console.ReadKey();
+        }
+    } }
+
