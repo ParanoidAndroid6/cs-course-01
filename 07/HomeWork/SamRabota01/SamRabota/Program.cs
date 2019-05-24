ef78ea469c22eae01c8ac9e44a86562c9855115e
Yura <kachiko888100@gmail.com>
2019-05-24 14:51:00 +0300

    Hm07

---
+++ 07/HomeWork/SamRabota01/SamRabota/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SamRabota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "    lorem   ipsum   dolor  sit    amet   ";
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] wordss = text.Split(' ');
+            words[1] = words[1].ToUpper();
+            var result1 = string.Join(' ', words);
+            Console.WriteLine(result1);
+
+            string text2 = text.TrimEnd();
+            int i = text2.LastIndexOf(' ');
+            text2 = text2.Substring(0, i);
+            text2 = text2.TrimEnd();
+            Console.WriteLine(text2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 07/HomeWork/Work1(1)/Work1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter minimum two words:");
+                var input = Console.ReadLine();
+                string[] i = input.Split(' ');
+                var u = 0;
+               foreach(string o in i)
+                {
+                   if (i.Length == 1)
+                    {
+                        Console.WriteLine("Error! Enter minimum two words please");
+                        break;
+                    }
+                  
+                   if (o.StartsWith("a"))
+                    {
+                        u++;
+                    }
+                   else if (o.StartsWith("A"))
+                    {
+                        u++;
+                    }
+                }
+
+                Console.WriteLine($"The count is {u}");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Error! Enter words, please");
+            }
+            Console.ReadKey();
+        }
+    }
+}
