5d7e92d37fece533c4ef82adaffccef63b9d8e37
Yura <kachiko888100@gmail.com>
2019-05-26 00:15:38 +0300

    HM07

---
+++ 07/HomeWork/Work2/ReversWord1/ReversWord/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReversWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter minimum one word:");
+            while (true)
+            {
+                var word = Console.ReadLine();
+                if (word == "")
+                {
+                    Console.WriteLine("Error! Enter something!");
+                    continue;
+                }
+                char[] wordReverse = word.ToCharArray();
+                Array.Reverse(wordReverse);
+                word = new string(wordReverse);
+                Console.WriteLine(word.ToLower());
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
