bf7c72094c2026508c2ac42db9fc9404a648f80f
Yura <kachiko888100@gmail.com>
2019-05-31 00:21:09 +0300

    HM08

---
+++ 08/HomeWork/ListHM01/ListHM/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ListHM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = true;
+            Stack<char> stk = new Stack<char>();
+            var input = Console.ReadLine();
+            char current, previous;
+            for (int i = 0; i < input.Length; i++)
+            {
+                current = input[i];
+                if (current == '(' || current == '[' || current == '{')
+                {
+                    stk.Push(current);
+                }
+                else if (current == ')' || current == ']' || current == '}')
+                {
+                    if (stk.Count == 0)
+                    {
+                        result = false;
+                    }
+
+                    else
+                    {
+                        previous = stk.Peek();
+                        if ((current == ')' && previous == '(') || (current == ']' && previous == '[') || (current == '}' && previous == '{'))
+                        {
+                            stk.Pop();
+                        }
+                        else
+                        {
+                            result = false;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+          
+        }
+    }
+}
