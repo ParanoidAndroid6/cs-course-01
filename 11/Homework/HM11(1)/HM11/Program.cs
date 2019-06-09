0eccc46dc0c7a3d5aaa760b814226eb7b9c5df95
Yura <kachiko888100@gmail.com>
2019-06-10 00:24:30 +0300

    HM11

--- 11/Homework/HM11(1)/HM11/Program.cs
+++ 11/Homework/HM11(1)/HM11/Program.cs
@@ -2,11 +2,31 @@
 
 namespace HM11
 {
-    class Program
+    class ReminderItem
     {
-        static void Main(string[] args)
+        public DateTimeOffset AlarmDate { get; set; }
+        public string AlarmMessage { get; set; }
+        public TimeSpan TimeToAlarm => TimeSpan.Parse($"{DateTime.Now - AlarmDate}");
+        public bool IsOutDated { get; set; }
+        public void WriteProperties()
         {
-            
+            Console.WriteLine($"Alarm date: {AlarmDate}, Alarm message: {AlarmMessage}, Time to alarm: {TimeToAlarm} ");
+        }
+        public ReminderItem()
+        {
+            AlarmDate = DateTime.Now.AddHours(3);
+            AlarmMessage = "WAKE UP!";
         }
     }
-}
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            var u = new ReminderItem();
+            u.WriteProperties();
+            Console.ReadKey();
+            }
+        }
+    }
+
